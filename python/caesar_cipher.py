2008526b69c779c1f2f9d3dc8e019ce84efc1d33
Jkim1122 <jkimz1122@gmail.com>
2023-10-11 21:34:38 -0500

    started

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,46 @@
 def caesar_cipher(string, shift_amount):
+    for i in range(0, len(string)):
+        # print(string[i])
+
+        if shift_amount == 0:
+            print(string[i])
+        # elif shift_amount > 0:
+        #     print(string[i + shift_amount])
+        # else:
+        #     print(string[i - shift_amount])
+
+
+caesar_cipher("abcdefg", 0)
+
+
+
+
+
+
+
+    # a = 1
+    # b = 2
+    # c = 3
+    # d = 4
+    # e = 5
+    # f = 6
+    # g = 7
+    # h = 8
+    # i = 9
+    # j = 10
+    # k = 11
+    # l = 12
+    # m = 13
+    # n = 14
+    # o = 15
+    # p = 16
+    # q = 17
+    # r = 18
+    # s = 19
+    # t = 20
+    # u = 21
+    # v = 22
+    # w = 23
+    # x = 24
+    # y = 25
+    # z = 26
